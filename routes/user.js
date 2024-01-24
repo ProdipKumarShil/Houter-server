d38516469dbe86b30c32c4d1ea4615ddc03628a5
ProdipKumarShil <prodipkrishna01@gmail.com>
2024-01-24 17:48:48 +0600

    user sign up

--- routes/user.js
+++ routes/user.js
@@ -5,5 +5,15 @@ const User = require('../models/user.model')
 router.get('/users', (req, res) => {
   res.send({user: 'all users'})
 })
+router.post('/signUp', async(req, res) => {
+  try {
+    const userData = req.body
+    const user = new User(userData)
+    await user.save()
+    res.status(201).send({ status: true, message: 'User created successfully!' })
+  } catch (error) {
+    res.status(500).send({status: false, message: error.message})
+  }
+})
 
 module.exports = router
